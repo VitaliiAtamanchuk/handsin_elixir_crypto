50b2942a45aceee12165abe0b22cfb97972fbb6a
Vitalii Atamanchuk <vitalii.atamanchuk3@gmail.com>
2023-01-30 11:42:20 +0200

    Added constant time compare

---
+++ README.md
@@ -0,0 +1,2 @@
+SRC - https://github.com/erlang/otp/tree/master/lib/crypto
+Doc - https://www.erlang.org/doc/man/crypto.html
--- hash_crypto_etc.exs
+++ hash_crypto_etc.exs
@@ -1,17 +1,32 @@
-msg = "Text"
-salt = "Salt"
-key = "Key"
+defmodule CryptoExamples do
+  import Bitwise
 
-IO.puts msg
+  def hash(msg) do
+    :crypto.hash(:sha256, msg)
+      |> Base.encode16
+  end
 
-hash = :crypto.hash(:sha256, msg)
-  |> Base.encode16
-IO.puts hash
+  def salted_hash(salt, msg) do
+    hash(msg <> salt)
+  end
 
-salted_hash = :crypto.hash(:sha256, msg <> salt)
-  |> Base.encode16
-IO.puts salted_hash
+  def hmac(key, msg) do
+    :crypto.mac(:hmac, :sha256, key, msg)
+      |> Base.encode16
+  end
 
-signature = :crypto.mac(:hmac, :sha256, key, msg)
-  |> Base.encode16
-IO.puts signature
+  # constant time compare
+  def compare?(a, b) when byte_size(a) != byte_size(b), do: false
+  def compare?(a, b) when is_binary(a) and is_binary(b) do
+    a_list = String.to_charlist(a)
+    b_list = String.to_charlist(b)
+
+    compare?(a_list, b_list)
+  end
+  def compare?(a, b) when is_list(a) and is_list(b) do
+    result = Enum.zip(a, b)
+      |> Enum.reduce(0, fn({i, j}, acc) -> acc ||| bxor(i, j) end)
+
+    result == 0
+  end
+end
